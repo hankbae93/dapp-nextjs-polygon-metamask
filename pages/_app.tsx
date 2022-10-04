03d9b2c6d3c5cd42c5a17f7704095606d75f09cd
Hank <77097180+ranjafunc@users.noreply.github.com>
2022-10-04 14:54:43 +0900

    remove library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,25 @@ import { RecoilRoot } from "recoil";
 import TransactionProvider from "src/provider/TransactionProvider";
 
 import { SessionProvider } from "next-auth/react";
-import { WagmiConfig, createClient } from "wagmi";
-import { getDefaultProvider } from "ethers";
+import { WagmiConfig, createClient, configureChains, chain } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+
 import { Session } from "next-auth";
+
+const { chains, provider } = configureChains(
+  [chain.polygonMumbai],
+  [publicProvider()]
+);
 
 const client = createClient({
   autoConnect: true,
-  provider: getDefaultProvider(),
+  provider,
+  connectors: [
+    new MetaMaskConnector({
+      chains,
+    }),
+  ],
 });
 
 function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
